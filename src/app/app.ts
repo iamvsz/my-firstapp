d874d43a394e519a0f5bdfb4206862a70958d7e2
Vishal Zade <vishalzade75@gmail.com>
2025-11-23 13:39:54 +0530

    template and styles use in component

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,8 +4,8 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  template: `<h1> Hello template </h1>`,
+  styles: `h1 { color: blue; }`
 })
 export class App {
   protected readonly title = signal('my-firstapp');
